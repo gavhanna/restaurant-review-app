05ec16b1cb38ff7354f2c70cdc8833099fabc75e
Gav Hanna <gavhanna@gmail.com>
2018-03-17 12:49:38 +0000

    refactor: added alt text for images

--- js/main.js
+++ js/main.js
@@ -141,7 +141,9 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `Photograph of ${restaurant.name}`;
   li.append(image);
+  
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
